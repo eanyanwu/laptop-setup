66644bb2f7ce27eb70d4e9b3bd218b7b70213baa
Eze Anyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-04-19 01:44:27 -0400

    Update environment.nix

--- nixos/environment.nix
+++ nixos/environment.nix
@@ -1,18 +1,4 @@
 { config, pkgs, ... }:
 
 {
-  # Environment
-  environment = {
-    etc = {
-      "sway/config" = {
-        source = ./laptop-setup/sway/config;
-      };
-      "sway/wallpaper.png" = {
-        source = ./laptop-setup/sway/wallpaper0.png;
-      };
-      "xdg/termite/config" = {
-        source = ./laptop-setup/termite/config;
-      };
-    };
-  };
 }
